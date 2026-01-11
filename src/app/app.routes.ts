8f7213b2c1346c1d0cbf687f5238512dd625abcf
kasia.kordys <kasia.kordys@gmail.com>
2026-01-11 20:26:53 +0200

    add app components and routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { HomeComponent } from './pages/home/home.component';
+import { LoginComponent } from './pages/login/login.component';
+import { ProjectsComponent } from './pages/projects/projects.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { StudentsComponent } from './pages/students/students.component';
+import { SubmitProjectComponent } from './pages/submit-project/submit-project.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'all-projects', component: ProjectsComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'students', component: StudentsComponent },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'submit-project', component: SubmitProjectComponent },
+];
