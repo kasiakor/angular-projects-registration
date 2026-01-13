92c538cd62d297cf9b4b0f4af38e425a47479083
kasia.kordys <kasia.kordys@gmail.com>
2026-01-13 21:06:27 +0200

    add register form and register api

--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -1,11 +1,41 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './register.component.html',
-  styleUrl: './register.component.css'
+  styleUrl: './register.component.css',
 })
 export class RegisterComponent {
+  registrationObject: any = {
+    fullName: '',
+    email: '',
+    password: '',
+    collegeName: '',
+    role: '',
+  };
 
+  http = inject(HttpClient);
+
+  onRegister() {
+    alert('Register button clicked');
+    debugger;
+    this.http
+      .post(
+        'https://api.freeprojectapi.com/api/ProjectCompetition/register',
+        this.registrationObject
+      )
+      .subscribe(
+        (res: any) => {
+          debugger;
+          alert('Registration successful');
+        },
+        (error) => {
+          console.error('Registration failed', error);
+          alert('Error: ' + (error.error?.message || error.statusText));
+        }
+      );
+  }
 }
