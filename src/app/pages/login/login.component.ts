ae2dbccaed7f2cfc8bcf6b5510e283c0c9f3c90c
kasia.kordys <kasia.kordys@gmail.com>
2026-01-14 22:37:27 +0200

    add login form with login service

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,11 +1,51 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { UserService } from '../../services/user.service';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrl: './login.component.css',
 })
 export class LoginComponent {
+  showPassword = false;
 
+  togglePassword() {
+    this.showPassword = !this.showPassword;
+  }
+
+  userService = inject(UserService);
+
+  loginForm: FormGroup = new FormGroup({
+    email: new FormControl(''),
+    password: new FormControl(''),
+  });
+
+  onLogin() {
+    alert('Login button clicked');
+    this.userService.onUserLogin(this.loginForm.value).subscribe(
+      (res: any) => {
+        debugger;
+        alert('Login successful');
+      },
+      (error) => {
+        console.error('Login failed', error);
+        alert('Error: ' + (error.error?.message || error.statusText));
+      }
+    );
+  }
+
+  // using new syntax
+  //   onLogin() {
+  //   this.userService.onUserLogin(this.loginForm.value).subscribe({
+  //     next: (res) => {
+  //       alert('Login successful');
+  //     },
+  //     error: (error) => {
+  //       console.error('Login failed', error);
+  //       alert('Error: ' + (error.error?.message || error.statusText));
+  //     },
+  //   });
+  // }
 }
