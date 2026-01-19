850152f626447639ce6aff4d639c7bfed147e868
kasia.kordys <kasia.kordys@gmail.com>
2026-01-19 20:42:26 +0200

    update home component to display all competitions

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,11 +1,18 @@
-import { Component } from '@angular/core';
+import { AsyncPipe, DatePipe } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { Observable } from 'rxjs/internal/Observable';
+import { Competition } from '../../interfaces/competition.interface';
+import { CompetitionService } from '../../services/competition.service';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [AsyncPipe, DatePipe],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
 })
 export class HomeComponent {
+  competitionSrv = inject(CompetitionService);
 
+  competitionsList$: Observable<Competition[]> =
+    this.competitionSrv.getCompetitions();
 }
